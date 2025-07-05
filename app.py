80783e87929d6e71c6866417998c32e1f863597e
andre <andrewahdev@gmail.com>
2025-07-05 01:44:43 -0400

    migraciones ejecutadas

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
 import os
 from dotenv import load_dotenv
 from models import db
@@ -12,6 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+migrate = Migrate(app, db) 
 
 # Importa los modelos después de inicializar db
 from models import Persona
