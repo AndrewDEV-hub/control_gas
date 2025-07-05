1e7d4e541b93759cf501dce5a0293f3e55cbe472
andre <andrewahdev@gmail.com>
2025-07-05 01:25:32 -0400

    creacion de .env y gitignore

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-
+from dotenv import load_dotenv
+from models import Persona
+load_dotenv()
 app = Flask(__name__)
 
 # Usa la variable de entorno DATABASE_URL que pondrás en Vercel
@@ -10,6 +12,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-@app.route("/")
-def home():
-    return "¡Hola desde Flask en Vercel!"
+@app.route("/test_db")
+def test_db():
+    persona = Persona.query.first()
+    if persona:
+        return f"Conectado a la base de datos. Primera persona: {persona.nombre}"
+    else:
+        return "Conectado a la base de datos, pero no hay personas."
